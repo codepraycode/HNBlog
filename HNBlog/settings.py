0d6b100b0baa4f46f3c1f686ac31fc996928ea49
Precious Olusola <preciousolusola16@gmail.com>
2023-01-07 16:07:16 +0100

    did Integration setup for vite

--- HNBlog/settings.py
+++ HNBlog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #Dependencies
-    'django_vite',
+    # 'django_vite',
     # Created apps
     'api.apps.ApiConfig',
 ]
