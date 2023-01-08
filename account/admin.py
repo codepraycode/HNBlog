1576f0bf2c69d3f9a9571349020e2c508beb511c
Precious Olusola <preciousolusola16@gmail.com>
2023-01-08 23:06:59 +0100

    Updated account admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from .models import UserAccount
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+@admin.register(UserAccount)
+class UserAccountAdmin(UserAdmin):
+    list_display = (
+        'username',
+        'email',
+        'is_staff',
+    )
+    
+    search_fields = (
+        'username',
+        'email'
+    )
+    
+    fieldsets = (
+        (_("User Info"), {
+            "fields": (
+                'username',
+                'email',
+                'password'
+            ),
+        }),
+    )
+    
