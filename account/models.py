ec73d6d5daae71adbe9a9c87726b590023b9e024
Precious Olusola <preciousolusola16@gmail.com>
2023-01-08 22:42:17 +0100

    Updated User account model

--- account/models.py
+++ account/models.py
@@ -1,6 +1,10 @@
 from helpers.models import BaseModel
 from django.utils.translation import gettext_lazy as _
 
+
+class UserAccountManager():
+    pass
+
 # Create your models here.
 class UserAccount(BaseModel):
     """
@@ -23,3 +27,28 @@ class UserAccount(BaseModel):
         _("User password"),
         max_length = 128,
     )
+    
+    # Meta data
+    is_verified = models.BooleanField(default = True) # in case of email verification
+    is_active = models.BooleanField(default = True) # if allowed to operate in app
+    is_staff = models.BooleanField(default = False) # if app admin user
+    date_joined = models.DateTimeField(auto_now_add=True,editable=True) # date account was created
+    last_login = models.DateTimeField(_('last login'), blank=True, null=True) # last time account authenticated
+    
+    
+    # Django Auth requirements
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS=['email']
+    
+    # Custom model manager
+    objects=UserAccountManager()
+    
+    # representation
+    def __str__(self):
+        return f"{self.username} - {self.email}"
+    
+    class Meta:
+        db_table = "accounts_tb"
+        verbose_name = "User account"
+        verbose_name_plural = "Users account"
+        
