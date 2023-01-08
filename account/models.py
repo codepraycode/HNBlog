b0036e5189e5c29847e36c9dd2d9604a5e016e13
Precious Olusola <preciousolusola16@gmail.com>
2023-01-08 22:36:46 +0100

    Created user model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+from helpers.models import BaseModel
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class UserAccount(BaseModel):
+    """
+        User Model
+    """
+    username = models.CharField(
+        _("Username"),
+        max_length = 255, 
+        blank = False, 
+        null = False,
+        unique = True, # should be unique
+    )
+    email = models.EmailField(
+        _("User email"),
+        max_length = 255, 
+        db_index = True,
+        unique = True, # should be unique
+    )
+    password = models.CharField(
+        _("User password"),
+        max_length = 128,
+    )
---
+++ helpers/model.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+"""
+    This file holds base models for all apps in project
+"""
+
+class BaseModel(models):
+    pass
